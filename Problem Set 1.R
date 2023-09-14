a56f0cb002752ab9fb083ee5333150480aed475b
Heidi Larsen <heidilinnemann@gmail.com>
2023-09-14 10:43:59 +0200

    Heidi Har lavet en fil
    Dumme Asger

---
+++ Problem Set 1.R
@@ -0,0 +1,18 @@
+dgp = function(n, p, beta = rep(1, (p+1)), mu_x = rep(0, p), Sigma_x = diag(rep(1,p)), sigma_eps = 1){
+  
+  # Libraries and functions:
+  require(mvtnorm)         # For multivariate normal.
+  
+  # Drawing predictors as multivariate normal:
+  X = rmvnorm(n, mean = mu_x, sigma = Sigma_x)
+  
+  # Noise is also normal:
+  eps = rnorm(n, 0, sigma_eps)
+  
+  # Outcome:
+  Y = cbind(rep(1, n), X) %*% beta + eps
+  
+  # Data:
+  data = data.frame(Y, X)
+  return(data)
+}
